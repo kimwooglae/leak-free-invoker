db2b73098e8b3dbd9d551b913f113acced6fbecd
kimwooglae <kimwooglae@gmail.com>
2013-06-08 00:36:51 +0900

    Update invoker.js

--- invoker.js
+++ invoker.js
@@ -5,20 +5,22 @@
 		for( i = 3 ; i < arguments.length ; i++ ) {
 			value = arguments[i];
 			switch (typeof value) {
-				case 'string':
+				case 'undefined':
 				case 'boolean':
-				case 'null':
 				case 'number':
-					args.push(1);	// not converted
+				case 'string':
+					args.push(typeof value);	// not converted
 					args.push(value);
 					break;
 				default:
-					if( isXML() ) {
-						args.push(2);	// XML
+					if (!value) {
+						args.push('null');	// null
+						args.push(null);
+					} else if( isXML() ) {
+						args.push('xml');	// XML
 						args.push(serilaize(value));
-						break;
 					} else {
-						args.push(3);  // JSON serialize object;
+						args.push('object');  // JSON serialize object;
 						args.push(JSON.stringify(value);
 					}
 			}
@@ -36,23 +38,25 @@
 		}
 		if( winObj) {
 			ret = winobj["method_callee"].call(null, args);
-			retType = ret[0];
-			retValue = ret.substring(1);
-			switch(retType) {
-				case "0":	// undefined
-					return undefined;
-				case "1":	// null
-					return null;
-				case "2":   // boolean 
-					return (retValue === 'true');
-				case "3":	// number
-					return Number(retValue);
-				case "4":   //  String
-					return retValue;
-				case "5":  // Object
-					return JSON.parse(retValue);
-				case "6":  // XML
-					return parse(retValue);
+			if(typeof ret === 'string') {
+				retType = ret[0];
+				retValue = ret.substring(1);
+				switch(retType) {
+					case "0":	// undefined
+						return undefined;
+					case "1":	// null
+						return null;
+					case "2":   // boolean 
+						return (retValue === 'true');
+					case "3":	// number
+						return Number(retValue);
+					case "4":   //  String
+						return retValue;
+					case "5":  // Object
+						return JSON.parse(retValue);
+					case "6":  // XML
+						return parse(retValue);
+				}
 			}
 		}
 	}
@@ -61,15 +65,60 @@
 		return false;
 	}
 	
-	var serialize = function(obj) {
+	var serializeXML = function(obj) {
 		return "";
 	}
 	
-	var method_callee = function() {
-	
-	
+	var parseXML = function(obj) {
+		
 	}
+	
+	var method_callee = function() {
+		if(arguments.length < 1) return;
+		var i,value, valeuType, args = [], method_name = arguments[0].split('.'), method_obj;
+		for( i = 1 ; i < arguments.length ; ) {
+			valueType = arguments[i++];
+			value = arguments[i++];
 
-
+			switch (typeof valueType) {
+				case 'undefined':
+				case 'null':
+				case 'boolean':
+				case 'number':
+				case 'string':
+					args.push(value);
+					break;
+				case 'object':
+					args.push(JSON.parse(value));
+					break;
+				case 'xml':
+					args.push(parseXML(value));
+					break;
+			}
+		}
+		method_obj = global[method_name[0]];
+		for(i=1;i<method_name.length;i++) {
+			method_obj = method_obj[method_name[i]];
+		}
+		value = methodObj.call(null, args);
+		switch (typeof value) {
+			case 'undefined':
+				return '0undefined';
+			case 'boolean':
+				return '2'+value;
+			case 'number':
+				return '3'+value;
+			case 'string':
+				return '4'+value;
+			default:
+				if(!value) {
+					return "1null";
+				} else if( isXML() ) {
+					return '6' + serilaizeXML(value);
+				} else {
+					return '5' + JSON.stringify(value);
+				}
+		}
+	}
 
 })(window)
